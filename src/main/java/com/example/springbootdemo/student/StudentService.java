d1055502874b40820ab68f5e11fd6eb9aa3c6b7f
Gerard Hynes <gkhynes@gmail.com>
2021-12-06 21:28:29 +0000

    Set up app for database connection

--- src/main/java/com/example/springbootdemo/student/StudentService.java
+++ src/main/java/com/example/springbootdemo/student/StudentService.java
@@ -1,9 +1,11 @@
 package com.example.springbootdemo.student;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
     public List<Student> getStudents(){
         return List.of(
