8e6bd38f2ed25e122f07c10ca3a3b0498f215072
Gerard Hynes <gkhynes@gmail.com>
2021-12-08 20:41:39 +0000

    Add JPA Repository

--- src/main/java/com/example/springbootdemo/student/StudentService.java
+++ src/main/java/com/example/springbootdemo/student/StudentService.java
@@ -1,4 +1,5 @@
 package com.example.springbootdemo.student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -7,11 +8,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L, "Mariam", "mariam.jamal@example.com", LocalDate.of(2000, Month.JANUARY, 5), 21
-                )
-        );
+        return studentRepository.findAll();
     }
 }
