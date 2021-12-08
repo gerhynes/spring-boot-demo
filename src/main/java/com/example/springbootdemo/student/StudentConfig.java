900d581f8f5e266fec523a2f475959ae59cb3239
Gerard Hynes <gkhynes@gmail.com>
2021-12-08 20:55:43 +0000

    Add students to DB

---
+++ src/main/java/com/example/springbootdemo/student/StudentConfig.java
@@ -0,0 +1,31 @@
+package com.example.springbootdemo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam.jamal@example.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21
+            );
+            Student alex = new Student(
+                    "Alex",
+                    "alexoneill@example.com",
+                    LocalDate.of(2002, Month.JANUARY, 5),
+                    19
+            );
+            repository.saveAll(List.of(mariam, alex));
+        };
+    }
+}
