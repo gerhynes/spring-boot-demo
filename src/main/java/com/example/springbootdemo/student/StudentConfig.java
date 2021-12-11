3a5a948f39d1141907f18422ce8a94686ef274cd
Gerard Hynes <gkhynes@gmail.com>
2021-12-11 11:22:27 +0000

    Calculate age instead of storing it

--- src/main/java/com/example/springbootdemo/student/StudentConfig.java
+++ src/main/java/com/example/springbootdemo/student/StudentConfig.java
@@ -16,14 +16,12 @@ public class StudentConfig {
             Student mariam = new Student(
                     "Mariam",
                     "mariam.jamal@example.com",
-                    LocalDate.of(2000, Month.JANUARY, 5),
-                    21
+                    LocalDate.of(2000, Month.JANUARY, 5)
             );
             Student alex = new Student(
                     "Alex",
                     "alexoneill@example.com",
-                    LocalDate.of(2002, Month.JANUARY, 5),
-                    19
+                    LocalDate.of(2002, Month.JANUARY, 5)
             );
             repository.saveAll(List.of(mariam, alex));
         };
